8db22fc076302e04f56d019c42f7989825db5fdd
cxllum <32437860+cxllum-xyz@users.noreply.github.com>
2017-12-10 11:44:44 +0000

    Update follow.js

--- follow.js
+++ follow.js
@@ -1,5 +1,5 @@
 var JQueryElement = document.createElement('script');
-JQueryElement.src = "http://code.jquery.com/jquery-latest.min.js";
+JQueryElement.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js";
 document.getElementsByTagName('head')[0].appendChild(JQueryElement);
 setTimeout(function() {
     $('._gexxb').each(function() {
